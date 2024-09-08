e3dfc86ae48f0a3f402a67c833c03bd92307a78c
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-09-08 04:00:28 -0300

    avance en guardado lectura sqlite + listview

--- app/src/main/java/com/example/grupo1_tp2/ContactosActivity.java
+++ app/src/main/java/com/example/grupo1_tp2/ContactosActivity.java
@@ -64,40 +64,43 @@ public class ContactosActivity extends AppCompatActivity {
         String consulta = "select * from contactos";
         Cursor fila = db.rawQuery(consulta, null);
         if (fila.moveToFirst()) {
-            Contacto contacto = new Contacto();
 
-            contacto.setNombre(fila.getString(0));
-            contacto.setApellido(fila.getString(1));
-            contacto.setTelefono(fila.getString(2));
-            contacto.setEmail(fila.getString(3));
-            contacto.setDireccion(fila.getString(4));
-            contacto.setFechaDeNacimiento(fila.getString(5));
-            contacto.setNivelDeEstudioAlcanzado(fila.getString(6));
-            contacto.setIntereses(fila.getString(7));
-            contacto.setNotificacion(fila.getString(8).equals("1")?true:false);
+            do {
+                Contacto contacto = new Contacto();
 
-            Log.d("LISTA", "8: "+fila.getString(8) + "---" + (fila.getString(8).equals("1")?true:false));
+                contacto.setNombre(fila.getString(0));
+                contacto.setApellido(fila.getString(1));
+                contacto.setTelefono(fila.getString(2));
+                contacto.setEmail(fila.getString(3));
+                contacto.setDireccion(fila.getString(4));
+                contacto.setFechaDeNacimiento(fila.getString(5));
+                contacto.setNivelDeEstudioAlcanzado(fila.getString(6));
+                contacto.setIntereses(fila.getString(7));
+                contacto.setNotificacion(fila.getString(8).equals("1") ? true : false);
 
-            listacontactos.add(contacto);
-            Toast.makeText(this, "Consulta exitosa: "+listacontactos.size(), Toast.LENGTH_SHORT).show();
+                Log.d("LISTA", "8: " + fila.getString(8) + "---" + (fila.getString(8).equals("1") ? true : false));
+
+                listacontactos.add(contacto);
+            } while (fila.moveToNext());
+            Toast.makeText(this, "Consulta exitosa: " + listacontactos.size(), Toast.LENGTH_SHORT).show();
         }
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.opcion1Agregar){
+        if (item.getItemId() == R.id.opcion1Agregar) {
             Toast.makeText(this, "Agregar contactos", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this, FormularioAgregarContacto.class));
         }
 
-        if(item.getItemId() == R.id.opcion2Listar){
+        if (item.getItemId() == R.id.opcion2Listar) {
             Toast.makeText(this, "Listado de contactos", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this, ContactosActivity.class));
         }
